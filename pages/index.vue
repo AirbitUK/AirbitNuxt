87c336ba3e68afdd9f8fff5afa36a712488533e3
AbdallahBedir <abdallahahmed2025@gmail.com>
2019-11-27 15:32:40 +0200

    [Chore] Require dotenv in nuxt.config

--- pages/index.vue
+++ pages/index.vue
@@ -40,8 +40,6 @@ export default {
   },
   loading: false,
   async asyncData({ $axios }) {
-    console.log(`axios`,$axios)
-    console.log(`API_URL`,process.env.API_URL)
     const latestReleases = {
       endpointUrl:
         '/beats/search?page=1&limit=12&search=&genre=0&moods=0&added=3&tempo=0&order=plays&free_download=0&marketplace=1&expand=user',
